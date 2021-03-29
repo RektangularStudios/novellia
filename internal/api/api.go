0793b7d0f61879654a08e6ed945b4fc25a5dc38b
Crystal Ball <concurrentdragon@gmail.com>
2021-03-28 20:02:29 -0700

    impl sdk

--- internal/api/api.go
+++ internal/api/api.go
@@ -36,8 +36,8 @@ func (s *ApiService) GetWallet(ctx context.Context, walletAddress string) (novel
 		})
 	return novellia_api.Response(200, tokens), nil
 
-	//TODO: Uncomment the next line to return response Response(400, {}) or use other options such as http.Ok ...
-	//return Response(400, nil),nil
+	//TODO: Uncomment the next line to return response novellia_api.Response(400, {}) or use other options such as http.Ok ...
+	//return novellia_api.Response(400, nil),nil
 
 	return novellia_api.Response(http.StatusNotImplemented, nil), errors.New("GetWallet method not implemented")
 }
@@ -47,8 +47,8 @@ func (s *ApiService) GetWorkflowMinterNvla(ctx context.Context) (novellia_api.Im
 	// TODO - update GetWorkflowMinterNvla with the required logic for this service method.
 	// Add api_default_service.go to the .openapi-generator-ignore to avoid overwriting this service implementation when updating open api generation.
 
-	//TODO: Uncomment the next line to return response Response(200, WorkflowInformation{}) or use other options such as http.Ok ...
-	//return Response(200, WorkflowInformation{}), nil
+	//TODO: Uncomment the next line to return response novellia_api.Response(200, WorkflowInformation{}) or use other options such as http.Ok ...
+	//return novellia_api.Response(200, WorkflowInformation{}), nil
 
 	return novellia_api.Response(http.StatusNotImplemented, nil), errors.New("GetWorkflowMinterNvla method not implemented")
 }
@@ -58,25 +58,25 @@ func (s *ApiService) PostCardanoTransaction(ctx context.Context, cardanoTransact
 	// TODO - update PostCardanoTransaction with the required logic for this service method.
 	// Add api_default_service.go to the .openapi-generator-ignore to avoid overwriting this service implementation when updating open api generation.
 
-	//TODO: Uncomment the next line to return response Response(200, {}) or use other options such as http.Ok ...
-	//return Response(200, nil),nil
+	//TODO: Uncomment the next line to return response novellia_api.Response(200, {}) or use other options such as http.Ok ...
+	//return novellia_api.Response(200, nil),nil
 
-	//TODO: Uncomment the next line to return response Response(400, {}) or use other options such as http.Ok ...
-	//return Response(400, nil),nil
+	//TODO: Uncomment the next line to return response novellia_api.Response(400, {}) or use other options such as http.Ok ...
+	//return novellia_api.Response(400, nil),nil
 
 	return novellia_api.Response(http.StatusNotImplemented, nil), errors.New("PostCardanoTransaction method not implemented")
 }
 
 // PostWorkflowMinterNvla -
-func (s *ApiService) PostWorkflowMinterNvla(ctx context.Context, minterRequest novellia_api.MinterRequest) (novellia_api.ImplResponse, error) {
+func (s *ApiService) PostWorkflowMinterNvla(ctx context.Context, minterInfo novellia_api.MinterInfo) (novellia_api.ImplResponse, error) {
 	// TODO - update PostWorkflowMinterNvla with the required logic for this service method.
 	// Add api_default_service.go to the .openapi-generator-ignore to avoid overwriting this service implementation when updating open api generation.
 
-	//TODO: Uncomment the next line to return response Response(200, CardanoTransaction{}) or use other options such as http.Ok ...
-	//return Response(200, CardanoTransaction{}), nil
+	//TODO: Uncomment the next line to return response novellia_api.Response(200, CardanoTransaction{}) or use other options such as http.Ok ...
+	//return novellia_api.Response(200, CardanoTransaction{}), nil
 
-	//TODO: Uncomment the next line to return response Response(400, {}) or use other options such as http.Ok ...
-	//return Response(400, nil),nil
+	//TODO: Uncomment the next line to return response novellia_api.Response(400, {}) or use other options such as http.Ok ...
+	//return novellia_api.Response(400, nil),nil
 
 	return novellia_api.Response(http.StatusNotImplemented, nil), errors.New("PostWorkflowMinterNvla method not implemented")
 }
